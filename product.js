730fcfffe5ba2a51e1a5a8faecc062ad45c29a99
Arun101019 <46786818+Arun101019@users.noreply.github.com>
2025-11-02 12:50:36 +0530

    Refactor product.js for improved debugging and quota handling

--- product.js
+++ product.js
@@ -178,3 +178,28 @@ function showProduct(index) {
 
   new bootstrap.Modal(document.getElementById("productModal")).show();
 }
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDRVCbBDzuqIG2mYBLDirReunh4oLJa5Wk",
+    authDomain: "thedecor-6d3f4.firebaseapp.com",
+    projectId: "thedecor-6d3f4",
+    storageBucket: "thedecor-6d3f4.firebasestorage.app",
+    messagingSenderId: "993017983175",
+    appId: "1:993017983175:web:3d5fcb9977236aa84b8ee2",
+    measurementId: "G-YT7CBME3LK"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+
